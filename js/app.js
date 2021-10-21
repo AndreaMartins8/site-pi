8c87f55e94a028d87eab98ee5510ed73e5e6cd7d
AndreaMartins8 <87030430+AndreaMartins8@users.noreply.github.com>
2021-10-20 21:30:50 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $("#slider").nivoSlider({
     controlNav: false,
 });
+
+let artes = ['imagens/abaporu.jpg', 'imagens/monalisa.jpg', 'imagens/obeijo.jpg']
+
+function CarregarArtes(){
+    let totalArtes = artes.length
+    let divListaArtes = document.querySelector(".lista-artes")
+
+    divListaArtes.innerHTML = ""
+    for (let i = 0; i <totalArtes; i++){
+    divListaArtes.innerHTML += "<img src=" + artes[i] +">"
+}
+}
